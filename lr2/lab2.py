ade3939c5c734718522a20eeedade1978ed3938e
npolubelov <hampval@gmail.com>
2020-10-16 21:17:14 +0700

    Создание класса и первого метода

--- lr2/lab2.py
+++ lr2/lab2.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
-import math
+class energy:
+ 
+    def __init__(self, name, price, energytype, objectof):
+        self.name = name
+        self.price = price  
+        self.energytype = energytype  
+        self.objectof = objectof  
+ 
+    def display_info(self):
+        print("Список энергосберегающих мероприятий:","\n")
+        print("Название - ",self.name)
+        print("Стоимость - ",self.price)
+        print("Тип затрагиваемого энергетического ресурса - ",self.energytype)
+        print("Объект, на котором будет проводится мероприятие - ",self.objectof,"\n")
+ 
+exmpl1 = energy("замена толпивоподающие системы", 545999,"мазут","Топливоподающая система")
+exmpl1.display_info()
