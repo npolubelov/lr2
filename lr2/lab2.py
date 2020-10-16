f160587ce9165d5b7aa0d0eb7e5023e1cd07a055
npolubelov <hampval@gmail.com>
2020-10-16 22:05:52 +0700

    Добавление параметров класса

--- lr2/lab2.py
+++ lr2/lab2.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 class energy:
  
-    def __init__(self, name, price, energytype, objectof):
+    def __init__(self, name, price, energytype, objectof, mbefore):
         self.name = name
         self.price = price  
         self.energytype = energytype  
-        self.objectof = objectof  
+        self.objectof = objectof
+        self.mdiffnce = mbefore
  
     def display_info(self):
-        print("Список энергосберегающих мероприятий:","\n")
-        print("Название - ",self.name)
-        print("Стоимость - ",self.price)
+        print("\n","Название энергосберегающего мероприятия - ",self.name)
+        print("Стоимость проведения мероприятия - ",self.price)
         print("Тип затрагиваемого энергетического ресурса - ",self.energytype)
-        print("Объект, на котором будет проводится мероприятие - ",self.objectof,"\n")
+        print("Объект, на котором будет проводится мероприятие - ",self.objectof)
+        print("Экономия на ресурсе в месяц после приминения мероприятия - ",self.mdiffnce,"\n")
  
-exmpl1 = energy("замена толпивоподающие системы", 545999,"мазут","Топливоподающая система")
+exmpl1 = energy("замена толпивоподающие системы", 545999,"мазут","Топливоподающая система",17300)
 exmpl1.display_info()
